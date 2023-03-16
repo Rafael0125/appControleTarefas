d10e935985fe41023b0f08a55aafd914d3236f4e
RAFAEL NAVARRO <rafanavarro0125@gmail.com>
2023-03-16 01:24:20 -0300

    update

--- appControleTarefas/src/app/app.module.ts
+++ appControleTarefas/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { TelaPrincipalComponent } from './tela-principal/tela-principal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    TelaPrincipalComponent
   ],
   imports: [
     BrowserModule,
